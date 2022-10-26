aa4b9153d3f6e976fa13e20d5401cd57dfb70c08
Colin Gohmann <colin.gohmann@yahoo.com>
2022-10-26 11:35:17 -0400

    add a compare analyses doc for comparing multiple insepectors on top of each other. Also Update installer

---
+++ POD Source Code/Docks/CompareAnalysesDoc.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace POD.Docks
+{
+    partial class CompareAnalysesDoc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WizardDock));
+            this.contextMenuStrip1 = new POD.Controls.ContextMenuStripConnected(this.components);
+            this.cutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.moveBarBox = new System.Windows.Forms.PictureBox();
+            this.movePanelBox = new System.Windows.Forms.PictureBox();
+            this.moveTitleBox = new System.Windows.Forms.PictureBox();
+            this.nextTitleBox = new System.Windows.Forms.PictureBox();
+            this.nextBarBox = new System.Windows.Forms.PictureBox();
+            this.nextPanelBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this._blendBox)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.moveBarBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.movePanelBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moveTitleBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nextTitleBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nextBarBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nextPanelBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _blendBox
+            // 
+            this._blendBox.Size = new System.Drawing.Size(634, 555);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.IsTextboxMenu = false;
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cutToolStripMenuItem,
+            this.copyToolStripMenuItem,
+            this.pasteToolStripMenuItem,
+            this.toolStripSeparator1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowOnlyButtons = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(103, 76);
+            // 
+            // cutToolStripMenuItem
+            // 
+            this.cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+            this.cutToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.cutToolStripMenuItem.Text = "Cut";
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.pasteToolStripMenuItem.Text = "Paste";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(99, 6);
+            // 
+            // moveBarBox
+            // 
+            this.moveBarBox.Location = new System.Drawing.Point(265, 160);
+            this.moveBarBox.Name = "moveBarBox";
+            this.moveBarBox.Size = new System.Drawing.Size(100, 50);
+            this.moveBarBox.TabIndex = 1;
+            this.moveBarBox.TabStop = false;
+            this.moveBarBox.Visible = false;
+            // 
+            // movePanelBox
+            // 
+            this.movePanelBox.Location = new System.Drawing.Point(325, 183);
+            this.movePanelBox.Name = "movePanelBox";
+            this.movePanelBox.Size = new System.Drawing.Size(100, 50);
+            this.movePanelBox.TabIndex = 2;
+            this.movePanelBox.TabStop = false;
+            this.movePanelBox.Visible = false;
+            // 
+            // moveTitleBox
+            // 
+            this.moveTitleBox.Location = new System.Drawing.Point(325, 269);
+            this.moveTitleBox.Name = "moveTitleBox";
+            this.moveTitleBox.Size = new System.Drawing.Size(100, 50);
+            this.moveTitleBox.TabIndex = 2;
+            this.moveTitleBox.TabStop = false;
+            this.moveTitleBox.Visible = false;
+            // 
+            // nextTitleBox
+            // 
+            this.nextTitleBox.Location = new System.Drawing.Point(340, 375);
+            this.nextTitleBox.Name = "nextTitleBox";
+            this.nextTitleBox.Size = new System.Drawing.Size(100, 50);
+            this.nextTitleBox.TabIndex = 2;
+            this.nextTitleBox.TabStop = false;
+            this.nextTitleBox.Visible = false;
+            // 
+            // nextBarBox
+            // 
+            this.nextBarBox.Location = new System.Drawing.Point(175, 422);
+            this.nextBarBox.Name = "nextBarBox";
+            this.nextBarBox.Size = new System.Drawing.Size(100, 50);
+            this.nextBarBox.TabIndex = 2;
+            this.nextBarBox.TabStop = false;
+            this.nextBarBox.Visible = false;
+            // 
+            // nextPanelBox
+            // 
+            this.nextPanelBox.Location = new System.Drawing.Point(410, 431);
+            this.nextPanelBox.Name = "nextPanelBox";
+            this.nextPanelBox.Size = new System.Drawing.Size(100, 50);
+            this.nextPanelBox.TabIndex = 2;
+            this.nextPanelBox.TabStop = false;
+            this.nextPanelBox.Visible = false;
+            // 
+            // WizardDock
+            // 
+            this.AllowEndUserDocking = false;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 555);
+            this.Controls.Add(this.nextPanelBox);
+            this.Controls.Add(this.nextBarBox);
+            this.Controls.Add(this.nextTitleBox);
+            this.Controls.Add(this.moveTitleBox);
+            this.Controls.Add(this.movePanelBox);
+            this.Controls.Add(this.moveBarBox);
+            this.DockAreas = WeifenLuo.WinFormsUI.Docking.DockAreas.Document;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HideOnClose = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "WizardDock";
+            this.Text = "WizardDock";
+            this.Activated += new System.EventHandler(this.WizardDock_Activated);
+            this.Controls.SetChildIndex(this._blendBox, 0);
+            this.Controls.SetChildIndex(this.moveBarBox, 0);
+            this.Controls.SetChildIndex(this.movePanelBox, 0);
+            this.Controls.SetChildIndex(this.moveTitleBox, 0);
+            this.Controls.SetChildIndex(this.nextTitleBox, 0);
+            this.Controls.SetChildIndex(this.nextBarBox, 0);
+            this.Controls.SetChildIndex(this.nextPanelBox, 0);
+            ((System.ComponentModel.ISupportInitialize)(this._blendBox)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.moveBarBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.movePanelBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moveTitleBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nextTitleBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nextBarBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nextPanelBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private POD.Controls.ContextMenuStripConnected contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.PictureBox moveBarBox;
+        private System.Windows.Forms.PictureBox movePanelBox;
+        private System.Windows.Forms.PictureBox moveTitleBox;
+        private System.Windows.Forms.PictureBox nextTitleBox;
+        private System.Windows.Forms.PictureBox nextBarBox;
+        private System.Windows.Forms.PictureBox nextPanelBox;
+    }
+}
---
+++ POD Source Code/Docks/CompareAnalysesDoc.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using POD.Wizards;
+using WeifenLuo.WinFormsUI.Docking;
+using Transitions;
+
+namespace POD.Docks
+{
+    //public delegate void SwapStepsEventHandler(object sender, SwapStepsEventArgs e);
+
+    /// <summary>
+    /// This is a Document Dock that holds a Step of a given Wizard. All Document Docks hold a Wizard Step.
+    /// </summary>
+    public partial class CompareAnalysesDoc : PodDock
+    {
+
+
+        #region Fields
+        /// <summary>
+        /// The Step that displays on the Dock.
+        /// </summary>
+        private WizardStep _step;
+        private bool _skipAnimations;
+        [NonSerialized]
+        public EventHandler NeedSteps = null;
+
+        public bool HasStep
+        {
+            get
+            {
+                return _step != null;
+            }
+        }
+
+        public bool SkipAnimations
+        {
+            get { return _skipAnimations; }
+            set
+            {
+                _skipAnimations = value;
+
+                if (_skipAnimations == true)
+                {
+                    Bitmap image = new Bitmap(Step.Width, Step.Height);
+
+                    Step.DrawPanelToBitmap(image, new Rectangle(0, 0, Step.Width, Step.Height));
+                    movePanelBox.Height = Step.Height;
+                    movePanelBox.Width = Step.Width;
+                    movePanelBox.Left = 0;
+                    movePanelBox.Top = 0;
+                    movePanelBox.Image = image;
+                    movePanelBox.BringToFront();
+                    movePanelBox.Show();
+
+                    Cursor.Current = Cursors.WaitCursor;
+                }
+                else
+                {
+                    movePanelBox.SendToBack();
+                    movePanelBox.Hide();
+
+                    Cursor.Current = Cursors.Default;
+                }
+
+
+            }
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Create a new Dock with the default Label.
+        /// </summary>
+        public CompareAnalysesDoc()
+        {
+            InitializeComponent();
+
+            Label = Globals.WizardDockLabel;
+            movePanelBox.SendToBack();
+            moveBarBox.SendToBack();
+
+            movePanelBox.Padding = new Padding(0, 0, 0, 0);
+            movePanelBox.Margin = new Padding(0, 0, 0, 0);
+
+            nextPanelBox.Padding = new Padding(0, 0, 0, 0);
+            nextPanelBox.Margin = new Padding(0, 0, 0, 0);
+        }
+        /// <summary>
+        /// Create a new Dock with a given Label.
+        /// </summary>
+        /// <param name="myLabel"></param>
+        public CompareAnalysesDoc(string myLabel)
+        {
+            InitializeComponent();
+
+            Label = myLabel;
+            Text = myLabel;
+
+            movePanelBox.SendToBack();
+            moveBarBox.SendToBack();
+
+            movePanelBox.Padding = new Padding(0, 0, 0, 0);
+            movePanelBox.Margin = new Padding(0, 0, 0, 0);
+        }
+
+        private SwapStepsEventArgs _args;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Get/set the current Wizard Step to display.
+        /// </summary>
+        public WizardStep Step
+        {
+            get
+            {
+                if(_step == null)
+                    AddSteps();
+
+                return _step; 
+            }
+            set
+            {
+                WizardStep temp = _step;
+
+                _step = value;
+
+                if (HasStep)
+                {
+                    movePanelBox.BringToFront();
+                    _step.Visible = false;
+                    Controls.Add(_step);
+                    _step.Dock = DockStyle.Fill;
+                    _step.SendToBack();
+                    _step.Visible = true;
+
+                    Text = _step.TabName;
+                    Label = _step.Source.Name;
+
+                    if (temp != _step && temp != null && Controls.Contains(temp) == true)
+                        Controls.Remove(temp);
+
+                    _step.PrepareGUI();
+                }
+
+                
+            }
+        }
+        /// <summary>
+        /// Get the Node whose children are to create a Step List for the Wizard Progress Dock.
+        /// </summary>
+        public TreeNode ProgressStepListNode
+        {
+            get
+            {
+                return _step.ProgressStepListNode;
+            }
+
+        }
+
+        public SwapStepsEventHandler SwapSteps;
+        #endregion
+
+        #region Methods
+        public void BringMovePanelToFront()
+        {
+            Bitmap image = new Bitmap(Step.Panel.Width, Step.Panel.Height);
+            Step.DrawPanelToBitmap(image, new Rectangle(Step.Panel.Left, Step.Panel.Top, Step.Panel.Width, Step.Panel.Height));
+            movePanelBox.Image = image;
+            movePanelBox.Height = Step.Panel.Height;
+            movePanelBox.Width = Step.Panel.Width;
+            movePanelBox.Left = 0;
+            movePanelBox.Top = 0;
+            movePanelBox.Visible = true;
+            movePanelBox.BringToFront();
+            movePanelBox.Show();
+        }
+        public void SendMovePanelToBack()
+        {
+            movePanelBox.SendToBack();
+            movePanelBox.Hide();
+            movePanelBox.Visible = false;
+            movePanelBox.Height = Step.Height;
+            movePanelBox.Width = Step.Width;
+            movePanelBox.Left = 0;
+            movePanelBox.Top = 0;
+        }
+        #endregion
+
+        #region Event Handling
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            _step.CloseOpenedContextMenu();
+        }
+
+        public void OnTransitionSteps(object sender, SwapStepsEventArgs args)
+        {
+            WizardStep wizTo = args.TransitionTo;
+            WizardStep wizFrom = args.TransitionFrom;
+            Transition transition;
+            Panel emptyPanel = new Panel();
+
+            emptyPanel.Height = Step.Height;
+            emptyPanel.Width = Step.Width;
+
+            if (wizTo != wizFrom)
+            {
+                //switch animation
+                transition = new Transition(new TransitionType_EaseInEaseOut(400));
+            }
+            else
+            {
+                //bounce animation
+                transition = new Transition(new TransitionType_Bounce(400));
+            }
+
+
+            transition.TransitionCompletedEvent += t_TransitionCompletedEvent;
+
+            _args = args;
+
+            wizTo.Height = wizFrom.Height;
+            wizTo.Width = wizFrom.Width;
+
+            wizFrom.Show();
+            wizTo.Show();
+
+            WizardStepAnimator panelAni = new WizardStepAnimator(transition, args);
+            WizardStepAnimator titleAni = new WizardStepAnimator(transition, args);
+            WizardStepAnimator barAni = new WizardStepAnimator(transition, args);
+
+            SuspendLayout();
+
+            titleAni.SetupSizes(moveTitleBox, nextTitleBox, wizFrom.TitleWidth, wizFrom.TitleHeight);
+            barAni.SetupSizes(moveBarBox, nextBarBox, wizFrom.BarWidth, wizFrom.BarHeight);
+            panelAni.SetupSizes(movePanelBox, nextPanelBox, wizFrom.PanelWidth, wizFrom.PanelHeight);
+
+            if (wizTo != wizFrom)
+            {
+
+                titleAni.SetupMoveLocations(wizTo.DrawTitleToBitmap, wizFrom.DrawTitleToBitmap, -wizFrom.TitleHeight, wizFrom.TitleLeft, wizFrom.TitleTop, wizFrom.TitleLeft);
+                titleAni.SetupNextLocations(wizTo.DrawTitleToBitmap, wizFrom.TitleTop, wizFrom.TitleLeft, args.Direction);
+
+                barAni.SetupMoveLocations(wizTo.DrawBarToBitmap, wizFrom.DrawBarToBitmap, wizFrom.Height, wizFrom.BarLeft, wizFrom.BarTop, wizFrom.BarLeft);
+                barAni.SetupNextLocations(wizTo.DrawBarToBitmap, wizFrom.BarTop, wizFrom.BarLeft, args.Direction);
+
+                panelAni.SetupMoveLocations(wizTo.DrawPanelToBitmap, wizFrom.DrawPanelToBitmap, wizFrom.PanelTop, wizFrom.PanelWidth, wizFrom.PanelTop, wizFrom.PanelLeft);
+                panelAni.SetupNextLocations(wizTo.DrawPanelToBitmap, wizFrom.PanelTop, wizFrom.PanelLeft, args.Direction);
+            }
+            else
+            {
+                int bounce = 6; //size of bounce inpixels
+                int bounce1;
+                int bounce2;
+                int bouncePrevPanel;
+                int bounceNextPanel;
+
+
+                if (args.Direction == 1)
+                {
+                    bounce1 = bounce;
+                    bounce2 = 0;
+                    bouncePrevPanel = bounce;
+                    bounceNextPanel = 0;
+                }
+                else
+                {
+                    bounce1 = 0;
+                    bounce2 = -bounce;
+                    bouncePrevPanel = 0;
+                    bounceNextPanel = -bounce;
+                }
+
+                titleAni.SetupMoveLocations(wizTo.DrawTitleToBitmap, wizFrom.DrawTitleToBitmap, wizFrom.TitleTop - bounce1, wizFrom.TitleLeft, wizFrom.TitleTop - bounce2, wizFrom.TitleLeft);
+                titleAni.SetupNextLocations(emptyPanel.DrawToBitmap, wizFrom.TitleTop, wizFrom.TitleLeft, 1);
+
+                barAni.SetupMoveLocations(wizTo.DrawBarToBitmap, wizFrom.DrawBarToBitmap, wizFrom.BarTop + bounce1, wizFrom.BarLeft, wizFrom.BarTop + bounce2, wizFrom.BarLeft);
+                barAni.SetupNextLocations(emptyPanel.DrawToBitmap, wizFrom.BarTop, wizFrom.BarLeft, 1);
+
+                panelAni.SetupMoveLocations(wizTo.DrawPanelToBitmap, wizFrom.DrawPanelToBitmap, wizFrom.PanelTop, wizFrom.PanelLeft-bouncePrevPanel, wizFrom.PanelTop, wizFrom.PanelLeft-bounceNextPanel);
+                panelAni.SetupNextLocations(emptyPanel.DrawToBitmap, wizFrom.PanelTop, wizFrom.PanelLeft, 1);
+
+                ResumeLayout();
+            }
+
+            titleAni.AddAnimations();
+            barAni.AddAnimations();
+            panelAni.AddAnimations();
+
+            ResumeLayout();
+
+            transition.run();
+        }
+
+        void t_TransitionCompletedEvent(object sender, Transition.Args e)
+        {
+            if (SwapSteps != null)
+                SwapSteps.Invoke(sender, _args);
+
+            //if (SkipAnimations == false)
+            //{
+            if (Controls.GetChildIndex(Step) != 0)
+                Step.BringToFront();
+            //}
+
+            movePanelBox.Hide();
+            moveBarBox.Hide();
+            moveTitleBox.Hide();
+            nextTitleBox.Hide();
+            nextBarBox.Hide();
+            nextPanelBox.Hide();
+        }
+
+        #endregion
+
+        public StepArgs CreateStepArgs()
+        {
+            if (_step == null)
+                return null;
+
+            StepArgs args = new StepArgs(_step.Index, _step.HelpFile, _step.ProgressStepListNode);
+
+            return args;
+        }
+
+
+        public void RefreshValues()
+        {
+            Step.RefreshValues();
+        }
+
+        private void WizardDock_Activated(object sender, EventArgs e)
+        {
+            _step.PrepareGUI();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            //see if actionbar can do something with the keys
+            var result = Step.ActionBar.SendKeys(keyData);
+
+            //if not then see if panel can do something with the keys
+            if(!result)
+                result = Step.Panel.SendKeys(keyData);
+
+            //if neither then pass the keys onward
+            if (result)
+                return result;
+            else
+                return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+
+        public void AddSteps()
+        {
+            if (NeedSteps != null)
+            {
+                NeedSteps.Invoke(this, null);
+            }
+        }
+    }
+}
