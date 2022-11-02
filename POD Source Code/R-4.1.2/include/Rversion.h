2018e38738d67b933184692815d050a6b084a2e6
Colin Gohmann <99298617+gohmann174442049@users.noreply.github.com>
2022-11-02 15:56:45 -0400

    Revert "remove more unnecessary components of R Engine"
    This reverts commit bb1b8191c2929853448acd8c46aae39514fce70e.

---
+++ POD Source Code/R-4.1.2/include/Rversion.h
@@ -0,0 +1,25 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 262402
+#define R_NICK "Bird Hippie"
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "4"
+#define R_MINOR  "1.2"
+#define R_STATUS ""
+#define R_YEAR   "2021"
+#define R_MONTH  "11"
+#define R_DAY    "01"
+#define R_SVN_REVISION 81115
+#define R_FILEVERSION    4,12,81115,0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
