dfa737d4b0ce37ab0f4d6a5cf7ea927e14a25903
ColinGohmann <colin.gohmann@yahoo.com>
2023-02-14 13:48:56 -0500

    Revert "remove extra metrics in SignalResponseMainAnalysisRObject. Will create a separate script to deal with the ghost POD curve thresholdchange"
    reverting the both commits for the ghost curve. All this work should be
    done on a separate branch
    This reverts commit dec0a9d1c0071e929304e029e1a13436e3c1f50f.

--- POD Source Code/RCode/RBackend/SignalResponseCode/SignalResponseMainAnalysisRObject.R
+++ POD Source Code/RCode/RBackend/SignalResponseCode/SignalResponseMainAnalysisRObject.R
@@ -59,6 +59,7 @@ AHatAnalysis<-setRefClass("AHatAnalysis", fields = list(signalRespDF="data.frame
                                                                         linearTestResults="list",
                                                                         aVPOD="matrix",
                                                                         ResultsPOD="data.frame",
+                                                                        ResultsPOD_All="data.frame",
                                                                         critPts="data.frame",
                                                                         #copyoutputfrompython
                                                                         residualTable="data.frame",
@@ -80,6 +81,12 @@ AHatAnalysis<-setRefClass("AHatAnalysis", fields = list(signalRespDF="data.frame
                                       )
                                       #POD curve table
                                       ResultsPOD<<-data.frame(
+                                        defect_sizes=c(1,1,1,1,1),
+                                        probabilities=c(0,0,0,0,0),
+                                        defect_sizes_upCI=c(0,0,0,0,0)
+                                      )
+                                      #PODCurveTableAll
+                                      ResultsPOD_All<<-data.frame(
                                         defect_sizes=c(1,1,1,1,1),
                                         probabilities=c(0,0,0,0,0),
                                         defect_sizes_upCI=c(0,0,0,0,0)
