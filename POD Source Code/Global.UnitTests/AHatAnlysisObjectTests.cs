9afe1464dd3c95bea644fd53e86dab5d42a8f48e
gohmann174442049 <colin.gohmann@yahoo.com>
2023-04-13 15:45:25 -0400

    Adding tests for AHatAnalysisObject

---
+++ POD Source Code/Global.UnitTests/AHatAnlysisObjectTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using POD;
+using CSharpBackendWithR;
+namespace Global.UnitTests
+{
+    [TestFixture]
+    public class AHatAnlysisObjectTests
+    {
+        private AHatAnalysisObject _ahatAnalysisObject;
+        private List<double> _flaws;
+        private Dictionary<string, List<double>> _reponseDict;
+        [SetUp]
+        public void Setup()
+        {
+            _ahatAnalysisObject = new AHatAnalysisObject();
+            _reponseDict = new Dictionary<string, List<double>>();
+            
+        }
+        ///<summary>
+        /// GetMaxFlaw() Test
+        /// </summary>
+        [Test]
+        public void GetMaxResponse_OneKeyValuePair_ReturnsMaxInsideDictionary()
+        {
+            List<double> responseList = new List<double>() { .1, .2, .3, .4, .5 };
+            _reponseDict.Add("Responses", responseList);
+            _ahatAnalysisObject.Responses_all = _reponseDict;
+
+            var result=_ahatAnalysisObject.GetMaxResponse();
+
+            Assert.That(result, Is.EqualTo(.5));
+        }
+        [Test]
+        public void GetMaxResponse_MulitpleKeyValuePairs_ReturnsMaxAmongAllTheDictionariesKeyValuePairs()
+        {
+            List<double> responseList = new List<double>() { .1, .2, .3, .4, .5 };
+            List<double> responseList2 = new List<double>() { .2, .4, .6, .8, 1.0 };
+            List<double> responseList3 = new List<double>() { .1, .15, .2, .25, .3 };
+            _reponseDict.Add("Responses", responseList);
+            _reponseDict.Add("Responses2", responseList2);
+            _reponseDict.Add("Responses3", responseList3);
+            _ahatAnalysisObject.Responses_all = _reponseDict;
+
+            var result = _ahatAnalysisObject.GetMaxResponse();
+
+            Assert.That(result, Is.EqualTo(1.0));
+        }
+
+    }
+}
