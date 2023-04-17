0356e9970438136e5e7143fc7a23173a44b74a57
gohmann174442049 <colin.gohmann@yahoo.com>
2023-04-17 15:36:32 -0400

    Tested rDataFrameToDataTable(). Maybe add custom exception handle for it later?

---
+++ POD Source Code/Global.UnitTests/PODObjectTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Data;
+using NUnit.Framework;
+using POD;
+using CSharpBackendWithR;
+namespace Global.UnitTests
+{
+    [TestFixture]
+    public class PODObjectTests
+    {
+        private PODObject _podObject;
+        [SetUp]
+        public void Setup()
+        {
+            _podObject = new PODObject();
+        }
+
+        /// No tests here, but need to seriously refactor the Get Accessror for WIzardType (many nested switch statements and almost impossible to read
+    }
+}
