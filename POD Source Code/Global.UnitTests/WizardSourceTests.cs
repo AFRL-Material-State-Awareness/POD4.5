39946570ce2db1c47d8262c80e5fc2a5353167c8
gohmann174442049 <colin.gohmann@yahoo.com>
2023-04-17 16:46:45 -0400

    Finished Unit test on the Global project (wizard source will have tests later)

---
+++ POD Source Code/Global.UnitTests/WizardSourceTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Data;
+using NUnit.Framework;
+using POD;
+using CSharpBackendWithR;
+namespace Global.UnitTests
+{
+    [TestFixture]
+    public class WizardSourceTests
+    {
+        //private Mock<WizardSource> _wizaardSource;
+        [SetUp]
+        public void Setup()
+        {
+           // _wizaardSource = new  Mock<WizardSource>();
+
+            
+        }
+        [Test]
+        public void SwitchHelpView_NeedSwitchHelpViewIsNull_NoInvocationThrown()
+        {
+            
+        }
+        /// TODO: Figure out how to make the methods in this abastract class testable
+    }
+}
