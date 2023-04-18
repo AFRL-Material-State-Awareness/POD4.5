f358693f52979efa00e939d77fd031a0d1b02f1e
gohmann174442049 <colin.gohmann@yahoo.com>
2023-04-18 09:44:49 -0400

    set up tests for Analyze project

---
+++ POD Source Code/Analyze.UnitTests/AnalysisTests.cs
@@ -0,0 +1,14 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using NUnit.Framework;
+namespace Analyze.UnitTests
+{
+    [TestFixture]
+    public class AnalysisTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
