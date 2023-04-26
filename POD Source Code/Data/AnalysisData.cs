fe0dfa2c010e1edd788e07874aa8d0a0c8f81761
Colin Gohmann <99298617+gohmann174442049@users.noreply.github.com>
2023-04-26 11:36:35 -0400

    Put Axis Object class in its own file (but the namespace will remain the same

--- POD Source Code/Data/AnalysisData.cs
+++ POD Source Code/Data/AnalysisData.cs
@@ -3827,27 +3827,6 @@ namespace POD.Data
         }
     }
     
-    public class AxisObject
-    {
-        public double Max = 1.0;
-        public double Min = -1.0;
-        public double IntervalOffset = 0.0;
-        public double Interval = .25;
-        public double BufferPercentage = 5.0;
-
-
-        public AxisObject Clone()
-        {
-            var axis = new AxisObject();
-
-            axis.Max = Max;
-            axis.Min = Min;
-            axis.IntervalOffset = IntervalOffset;
-            axis.Interval = Interval;
-            axis.BufferPercentage = BufferPercentage;
-
-            return axis;
-        }
-    }
+    
 }
 
