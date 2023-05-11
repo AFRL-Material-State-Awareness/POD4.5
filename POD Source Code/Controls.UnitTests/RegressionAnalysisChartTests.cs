d0b494c01e0c842ab7f2f9bf660d2ef97084bcb5
gohmann174442049 <colin.gohmann@yahoo.com>
2023-05-11 12:21:24 -0400

    Created unit tests for DrawEquation, DrawAMaxBoundLine, DrawAMinBoundLine, and DrawThresholdBoundLine functions

--- POD Source Code/Controls.UnitTests/RegressionAnalysisChartTests.cs
+++ POD Source Code/Controls.UnitTests/RegressionAnalysisChartTests.cs
@@ -11,17 +11,30 @@ using POD;
 using System.Reflection;
 using System.Drawing;
 using NUnit.Framework.Constraints;
+using System.Windows.Forms.DataVisualization.Charting;
+using POD.Data;
+using System.Data;
 
 namespace Controls.UnitTests
 {
     [TestFixture]
     public class RegressionAnalysisChartTests
     {
-        private RegressionAnalysisChart _regressionAnalysisChart;
+        private FakeRegressionAnalysisChart _regressionAnalysisChart;
+        private Mock<IAnalysisData> _data;
         [SetUp]
         public void Setup()
         {
-            _regressionAnalysisChart = new RegressionAnalysisChart();
+            _regressionAnalysisChart = new FakeRegressionAnalysisChart();
+            SetUpAnalysisDataObject();
+        }
+        private void SetUpAnalysisDataObject()
+        {
+            _data = new Mock<IAnalysisData>();
+            _data.Setup(d => d.ActivatedFlaws).Returns(new DataTable());
+            _data.Setup(d => d.ActivatedResponses).Returns(new DataTable());
+            _data.Setup(d => d.ActivatedSpecimenIDs).Returns(new DataTable());
+            _regressionAnalysisChart.Data= _data.Object;
         }
         /// Tests for the List<ToolStripItem> BuildMenuItems(double x, double y) function
         [Test]
@@ -83,6 +96,148 @@ namespace Controls.UnitTests
         {
             using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Controls.UnitTests.Resources.ShowNormalitySample.png"))
                 return new Bitmap(stream);
+        }
+
+        /// Tests for the void DrawEquation function
+        [Test]
+        public void DrawEquation_EquationIsNull_EquationAndBoxNotAddedToAnnotations()
+        {
+            //Arrange
+            _regressionAnalysisChart.Equation = null;
+            //Act
+            _regressionAnalysisChart.DrawEquation();
+            //Assert
+            Assert.That(_regressionAnalysisChart.Annotations.Count, Is.EqualTo(2));
+        }
+        [Test]
+        public void DrawEquation_EquationIsNotNull_EquationAndBoxNotAddedToAnnotations()
+        {
+            //Arrange
+            _regressionAnalysisChart.Equation = new TextAnnotation();
+            //Act
+            _regressionAnalysisChart.DrawEquation();
+            //Assert
+            Assert.That(_regressionAnalysisChart.Annotations.Count, Is.Zero);
+        }
+        /// Tests for the void DrawAMaxBoundLine function
+        [Test]
+        public void DrawAMaxBoundLine_AMaxLineIsNull_AMaxLineAddedToAnnotations()
+        {
+            //Arrange
+            _regressionAnalysisChart.AMaxLine = null;
+            //Act
+            _regressionAnalysisChart.DrawAMaxBoundLine();
+            //Assert
+            Assert.That(_regressionAnalysisChart.Annotations.Count, Is.EqualTo(1));
+            _data.VerifyGet(d => d.ActivatedFlaws);
+        }
+        [Test]
+        public void DrawAMaxBoundLine_AMaxLineIsNotNull_NotAddedToAnnotations()
+        {
+            //Arrange
+            _regressionAnalysisChart.AMaxLine = new VerticalLineAnnotation();
+            //Act
+            _regressionAnalysisChart.DrawAMaxBoundLine();
+            //Assert
+            Assert.That(_regressionAnalysisChart.Annotations.Count, Is.Zero);
+        }
+        /// Tests for the void DrawAMinBoundLine function
+        [Test]
+        public void DrawAMinBoundLine_AMinLineIsNull_AMaxLineAddedToAnnotations()
+        {
+            //Arrange
+            _regressionAnalysisChart.AMinLine = null;
+            //Act
+            _regressionAnalysisChart.DrawAMinBoundLine();
+            //Assert
+            Assert.That(_regressionAnalysisChart.Annotations.Count, Is.EqualTo(1));
+            _data.VerifyGet(d => d.ActivatedFlaws);
+        }
+        [Test]
+        public void DrawAMinBoundLine_AMinLineIsNotNull_NotAddedToAnnotations()
+        {
+            //Arrange
+            _regressionAnalysisChart.AMinLine = new VerticalLineAnnotation();
+            //Act
+            _regressionAnalysisChart.DrawAMinBoundLine();
+            //Assert
+            Assert.That(_regressionAnalysisChart.Annotations.Count, Is.Zero);
+        }
+        /// Tests for the void DrawThresholdBoundLine function
+        [Test]
+        public void DrawThresholdBoundLine_ThresholdLineIsNull_ThresholdLineAddedToAnnotations()
+        {
+            //Arrange
+            _regressionAnalysisChart.ThresholdLine = null;
+            //Act
+            _regressionAnalysisChart.DrawThresholdBoundLine();
+            //Assert
+            Assert.That(_regressionAnalysisChart.Annotations.Count, Is.EqualTo(1));
+        }
+        [Test]
+        public void DrawThresholdBoundLine_ThresholdLineIsNotNull_NotAddedToAnnotations()
+        {
+            //Arrange
+            _regressionAnalysisChart.ThresholdLine = new HorizontalLineAnnotation();
+            //Act
+            _regressionAnalysisChart.DrawThresholdBoundLine();
+            //Assert
+            Assert.That(_regressionAnalysisChart.Annotations.Count, Is.Zero);
+        }
+        [Test]
+        public void DrawThresholdBoundLine_ThresholdFreezeIsTrue_AllowMovingFalseAndSetToFreezeValue()
+        {
+            //Arrange
+            var freezeValue = 1.0;
+            _regressionAnalysisChart.ThresholdLine = new HorizontalLineAnnotation();
+            _regressionAnalysisChart.ThresholdLine.AllowMoving = true;
+            _regressionAnalysisChart.FreezeThresholdLine(freezeValue);
+            //Act
+            _regressionAnalysisChart.DrawThresholdBoundLine();
+            //Assert
+            Assert.That(_regressionAnalysisChart.ThresholdLine.AllowMoving, Is.False);
+            Assert.That(_regressionAnalysisChart.ThresholdLine.Y, Is.EqualTo(freezeValue));
+        }
+        [Test]
+        public void DrawThresholdBoundLine_ThresholdFreezeIsFalse_AllowMovingTrueAndNotSetToFreezeValue()
+        {
+            //Arrange
+            _regressionAnalysisChart.ThresholdLine = new HorizontalLineAnnotation();
+            _regressionAnalysisChart.ThresholdLine.AllowMoving = true;
+            //Act
+            _regressionAnalysisChart.DrawThresholdBoundLine();
+            //Assert
+            Assert.That(_regressionAnalysisChart.ThresholdLine.AllowMoving, Is.True);
+            Assert.That(_regressionAnalysisChart.ThresholdLine.Y, Is.EqualTo(double.NaN));
+        }
+
+    }
+    public class FakeRegressionAnalysisChart : RegressionAnalysisChart
+    {
+        public TextAnnotation Equation
+        {
+            set { _equation = value; }
+        }
+        public VerticalLineAnnotation AMaxLine
+        {
+            set { _aMaxLine = value; }
+        }
+        public VerticalLineAnnotation AMinLine
+        {
+            set { _aMinLine = value; }
+        }
+        public HorizontalLineAnnotation ThresholdLine
+        {
+            set { _thresholdLine = value; }
+            get { return this._thresholdLine; }
+        }
+        public IAnalysisData Data
+        {
+            set { _analysisData = value; }
+        }
+        private double GetInitialAMaxLocationFromData()
+        {
+            return 0.0;
         }
     }
 }
