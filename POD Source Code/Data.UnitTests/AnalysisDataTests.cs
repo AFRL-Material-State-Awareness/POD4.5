8bacc176c076ce149af40aca8ee462dfa2e26631
gohmann174442049 <colin.gohmann@yahoo.com>
2023-05-17 14:36:53 -0400

    Adding unit Test project for Data.

---
+++ POD Source Code/Data.UnitTests/AnalysisDataTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using Moq;
+namespace Data.UnitTests
+{
+    [TestFixture]
+    public class AnalysisDataTests
+    {
+        private AnalysisData _data;
+        /// <summary>
+        /// ActivatedFlawName
+        /// </summary>
+        [Test]
+        public void ActivatedFlawName_ActivatedFlawCountIsZero_ReturnsEmptyString()
+        {
+
+        }
+    }
+}
