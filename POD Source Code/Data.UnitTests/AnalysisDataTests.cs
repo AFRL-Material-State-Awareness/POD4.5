f84cf295d11a3062337ab3d30f794439e6e40f78
gohmann174442049 <colin.gohmann@yahoo.com>
2023-05-18 09:27:59 -0400

    Finished Unit tests for ActivatedFlawName and ActivatedOriginalFlawName

--- POD Source Code/Data.UnitTests/AnalysisDataTests.cs
+++ POD Source Code/Data.UnitTests/AnalysisDataTests.cs
@@ -2,6 +2,7 @@
 using System;
 using Moq;
 using POD.Data;
+using System.Collections.Generic;
 
 namespace Data.UnitTests
 {
@@ -9,13 +10,58 @@ namespace Data.UnitTests
     public class AnalysisDataTests
     {
         private AnalysisData _data;
+        [SetUp]
+        public void Setup()
+        {
+            _data = new AnalysisData();
+        }
         /// <summary>
-        /// ActivatedFlawName
+        /// tests ActivatedFlawName Getter
         /// </summary>
         [Test]
         public void ActivatedFlawName_ActivatedFlawCountIsZero_ReturnsEmptyString()
         {
-
+            //Arrange
+            //Act
+            var result = _data.ActivatedFlawName;
+            //Assert
+            Assert.AreEqual(result, string.Empty);
+        }
+        [Test]
+        public void ActivatedFlawName_ActivatedFlawCountGreaterThanZero_ReturnsActivatedFlawName()
+        {
+            //Arrange
+            DataSource source = new DataSource("MyDataSource", "ID", "flawName.centimeters", "Response");
+            _data.SetSource(source);
+            //Act
+            var result = _data.ActivatedFlawName;
+            //Assert
+            Assert.AreEqual(result, "flawName.centimeters");
+        }
+        /// <summary>
+        /// tests ActivatedOriginalFlawName Getter
+        /// </summary>
+        [Test]
+        public void ActivatedOriginalFlawName_NamesCountIsZero_ReturnsEmptyString()
+        {
+            //Arrange
+            //Act
+            var result = _data.ActivatedOriginalFlawName;
+            //Assert
+            Assert.AreEqual(result, string.Empty);
+        }
+        [Test]
+        public void ActivatedOriginalFlawName_NamesCountGreaterThanZero_ReturnsActivatedFlawName()
+        {
+            //Arrange
+            DataSource source = new DataSource("MyDataSource", "ID", "flawName.centimeters", "Response");
+            _data.SetSource(source);
+            _data.ActivateFlaw("flawName.centimeters");
+            //_data.ActivateFlaws(new List<string>());
+            //Act
+            var result = _data.ActivatedOriginalFlawName;
+            //Assert
+            Assert.AreEqual(result, "flawName.centimeters");
         }
     }
 }
