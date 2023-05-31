0716f52fddec9d4a7ea7229478a9f0c14cc1d0d1
Colin Gohmann <99298617+gohmann174442049@users.noreply.github.com>
2023-05-31 15:43:35 -0400

    Added utility function that controls AddRowToTable. Also included an interface for unit testing purposes

---
+++ POD Source Code/Data/AddRowToTableControl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class AddRowToTableControl : IAddRowToTableControl
+    {
+        public void AddStringRowToTable(string myID, int index, DataTable table)
+        {
+            if (table.Rows.Count <= index)
+            {
+                var row = table.NewRow();
+
+                row[0] = myID;
+
+                table.Rows.Add(row);
+            }
+            else
+            {
+                table.Rows[index][0] = myID;
+            }
+        }
+
+        public void AddDoubleRowToTable(double myValues, int index, DataTable table)
+        {
+            if (table.Rows.Count <= index)
+            {
+                var row = table.NewRow();
+
+                row[0] = myValues;
+
+                table.Rows.Add(row);
+            }
+            else
+            {
+                table.Rows[index][0] = myValues;
+            }
+        }
+    }
+    public interface IAddRowToTableControl
+    {
+        void AddStringRowToTable(string myID, int index, DataTable table);
+        void AddDoubleRowToTable(double myValues, int index, DataTable table);
+    }
+}
