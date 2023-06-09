4fe5db46de11e6a4577ee451b1685c259f1401b7
gohmann174442049 <colin.gohmann@yahoo.com>
2023-06-09 11:09:14 -0400

    remove commented out code in globals.

--- POD Source Code/POD/Globals.cs
+++ POD Source Code/POD/Globals.cs
@@ -1266,18 +1266,6 @@ namespace POD
 
         public static string GetDefaultValue(string colType)
         {
-            /*
-            if (colType == ExtColProperty.Unit)
-                return ExtColProperty.UnitDefault;
-            else if (colType == ExtColProperty.Max)
-                return ExtColProperty.MaxDefault.ToString();
-            else if (colType == ExtColProperty.Min)
-                return ExtColProperty.MinDefault.ToString();
-            else if (colType == ExtColProperty.Thresh)
-                return ExtColProperty.ThreshDefault.ToString();
-            else
-                return "";
-            */
             switch (colType)
             {
                 case ExtColProperty.Unit:
